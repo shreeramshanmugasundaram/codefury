2f45b45df9f3af4a089e691640bfab1eeafff05e
Nikhil Patil <nikhil.patil_entc21@pccoer.in>
2023-09-24 07:14:20 +0530

    rooms

--- server/controllers/mentor/createroom.js
+++ server/controllers/mentor/createroom.js
@@ -5,38 +5,19 @@ import roomSchema from "../../models/roomSchema.js";
 const createroom = async(req,res)=>{
     console.log(req.body);
     try {
-      const { email, password, cpassword , phone, qualification } = req.body;
-      if (!email || !password || !cpassword || !phone || !qualification) {
+      const {roomname} = req.body;
+      if (!roomname) {
         return res
           .status(400)
           .json({ message: "Please fill all the required inputs " });
       }
-      if (password !== cpassword) {
-        return res.status(400).json({ message: "Password does not match" });
-      }
-      const existingUser = await doctorSchema.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ message: "User already Exist." });
-      }
   
-      const hashedPassword = await bcrypt.hash(password, 12);
-  
-      const result = await doctorSchema.create({
-        email,
-        password: hashedPassword,
-        phone:phone,
-        qualification:qualification
+      const result = await roomSchema.create({
+        roomname
       });
-  
-      const jsonKey = process.env.JSONTOKEN;
-      const token = jwt.sign({ phone: result.phone, id: result._id }, jsonKey, {
-        expiresIn: "24h",
-      });
-  
       return res.status(201).json({
-        result: { email },
-        token,
-        message: "Account Created Successfully",
+        result: { roomname },
+        message: "room Created Successfully",
       });
     } catch (error) {
       console.log(error);
@@ -47,4 +28,4 @@ const createroom = async(req,res)=>{
     }
 
 }
-export default registerdoctor; 
+export default createroom; 
